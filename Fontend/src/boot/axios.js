1eacddeef70d9b39dd638836c3ad970d461ff414
Vajiranga Pathirana <yvajiranga16@gmail.com>
2026-01-13 21:09:42 +0530

    Update API URL for production

--- Fontend/src/boot/axios.js
+++ Fontend/src/boot/axios.js
@@ -1,7 +1,7 @@
 import { boot } from 'quasar/wrappers';
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'http://localhost:8000/api' });
+const api = axios.create({ baseURL: 'https://nurya-project-ba-git-80a5eb-a-p-y-vajiranga-pathiranas-projects.vercel.app/api' });
 
 export default boot(({ app }) => {
   app.config.globalProperties.$axios = axios;
