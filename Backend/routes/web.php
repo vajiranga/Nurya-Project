0f119b93a9a48f6bc7299873b8cd706473d676b2
Vajiranga Pathirana <yvajiranga16@gmail.com>
2026-01-13 20:52:50 +0530

    Fix vercel config and add migration route

--- Backend/routes/web.php
+++ Backend/routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/migrate', function () {
+    try {
+        Artisan::call('migrate:fresh --seed --force');
+        return 'Migration and Seeding Completed Successfully!';
+    } catch (\Exception $e) {
+        return 'Error: ' . $e->getMessage();
+    }
 });
